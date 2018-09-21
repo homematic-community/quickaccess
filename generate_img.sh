9c579bf0f9648a8825fb89151c424b9cb84940df
Jens Maus <mail@jens-maus.de>
2018-09-21 15:13:22 +0200

    fix installation routines for RaspberryMatic

--- generate_img.sh
+++ generate_img.sh
@@ -4,6 +4,7 @@ cp -a qa/* tmp/qa
 cp update_script tmp/
 cp quickaccess tmp/
 cd tmp
-tar --owner=root --group=root --exclude=.DS_Store -czvf ../quickaccess-2.1b2.tar.gz *
+find . -not -name '.DS_Store' -not -name '*.sha256' -type f -print0 | xargs -0 sha256sum >quickaccess-2.1b3.sha256
+tar --owner=root --group=root --exclude=.DS_Store -czvf ../quickaccess-2.1b3.tar.gz *
 cd ..
 rm -rf tmp
